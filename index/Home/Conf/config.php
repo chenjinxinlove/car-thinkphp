e9e1c7eaa2d42089f6eb144873ed1293617945ff
chenjinxinlove <505150704@qq.com>
2016-02-22 09:42:32 +0800

    add data

--- index/Home/Conf/config.php
+++ index/Home/Conf/config.php
@@ -1,4 +1,10 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	'DB_TYPE' => 'mysql',
+	'DB_HOST' => 'localhost',
+	'DB_NAME' => 'gwch',
+	'DB_USER' => 'root',
+	'DB_PWD'  => 'root',
+	'DB_PREFIX' => 'car_'
 );
