60443df43dcc83187cb16e8187b8a55639548f6e
chenjinxinlove <505150704@qq.com>
2016-02-25 10:07:53 +0800

    add alertuser`

--- index/Home/Controller/UserController.class.php
+++ index/Home/Controller/UserController.class.php
@@ -57,23 +57,38 @@ class UserController extends CommonController {
         
    } 
    //增加用户
-   public function adduser () 
+   public function add () 
    {
         $this -> display();
-        // if (!IS_POST) {
-        //     $this -> error('页面不存在');
-        // }
-        // $title = I('post.title','','htmlspecialchars');
-        // $content = I('post.content','','htmlspecialchars');
-        // $data['time'] = time();
-        // $data['intro'] = $title;
-        // $data['content'] = $content;
-        // if (M('user') -> add($data)) {
-        //     $this -> success ('新增成功！');
-        // }
-        // $this -> error ('新增失败！');
+      
    }
-
+   public function addUser ()
+    {
+        if (!IS_POST) {
+            $this -> error('页面不存在');
+        }
+        $username = I('post.username','','htmlspecialchars');
+        $password= I('post.password','','htmlspecialchars');
+        $password1= I('post.password1','','htmlspecialchars');
+        if ($password === $password1) {
+          $pwd = md5($password);
+        }else{
+          $this ->error("两次密码不一致");die;
+        }
+        $section = I('post.section','','htmlspecialchars');
+        $tel = I('post.tel','','htmlspecialchars');
+        $pock = I('post.pock','','htmlspecialchars');
+        $data['time'] = time();
+        $data['username'] = $username;
+        $data['pwd'] = $pwd;
+        $data['section'] = $section;
+        $data['tel'] = $tel;
+        $data['pock'] = $pock;
+        if (M('user') -> add($data)) {
+            $this -> success ('新增成功！');die;
+        }
+        $this -> error ('新增失败！');
+   }
    //修改用户
    public function alert ()
    {
@@ -83,5 +98,21 @@ class UserController extends CommonController {
         $this->assign('id' ,$id);
         $this ->assign('content' , $content);
         $this -> display();
+   }
+   public function alertinfo ()
+    {   
+      // p($_POST);die;
+        $password= md5(I('post.password','','htmlspecialchars'));
+        $section = I('post.section','','htmlspecialchars');
+        $tel = I('post.tel','','htmlspecialchars');
+        $data['time'] = time();
+        $data['pwd'] = $password;
+        $data['section'] = $section;
+        $data['tel'] = $tel;
+        $where['id'] = I('get.id','','htmlspecialchars');
+        if (M('user') -> where($where) ->save($data)) {
+            $this -> success ('修改成功！');die;
+        }
+        $this -> error ('修改失败！');
    }
 }
